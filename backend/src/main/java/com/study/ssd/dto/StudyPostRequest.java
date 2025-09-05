491ff2e8e59de271935ad0a7a8af33d6979e61f7
dhrxh99 <dhrxhan@gmail.com>
2025-09-05 18:08:12 +0900

    게시글 유효성 검사 dto 생성

--- backend/src/main/java/com/study/ssd/dto/StudyPostRequest.java
+++ backend/src/main/java/com/study/ssd/dto/StudyPostRequest.java
@@ -1,4 +1,23 @@
 package com.study.ssd.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class StudyPostRequest {
+
+    @NotBlank(message = "제목을 입력하세요.")
+    private String title;
+
+    @NotBlank(message = "내용을 입력하세요.")
+    private String content;
+
+    @NotBlank(message = "카테고리를 입력하세요.")
+    private String category;
+
+    @NotBlank(message = "참여인원을 입력하세요.")
+    @Max(value = 50, message = "최대 인원은 50명입니다.")
+    private int maxCount;
 }
