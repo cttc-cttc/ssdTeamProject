6314c2314edd69e5bb7a62114917013647dbc7fb
dhrxh99 <dhrxhan@gmail.com>
2025-09-05 18:07:21 +0900

    게시글 유효성 검사 dto 생성

--- backend/src/main/java/com/study/ssd/dto/StudyPostResponse.java
+++ backend/src/main/java/com/study/ssd/dto/StudyPostResponse.java
@@ -1,4 +1,22 @@
 package com.study.ssd.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class StudyPostResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String category;
+    private LocalDateTime deadline;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+    private int currentCont = 0;
+    private int maxCount = 0;
+    private int wishCount = 0;
 }
