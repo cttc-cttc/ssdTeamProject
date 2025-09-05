821d077ec9dc2d2dc4041fac1a142bf7109b8cdc
dhrxh99 <dhrxhan@gmail.com>
2025-09-05 12:31:22 +0900

    studyPost Entity clear

--- backend/src/main/java/com/study/ssd/entity/StudyPost.java
+++ backend/src/main/java/com/study/ssd/entity/StudyPost.java
@@ -1,4 +1,39 @@
 package com.study.ssd.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "study_post")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudyPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+
+    @Column(length = 100, nullable = false)
+    private  String title;
+
+    @Column(length = 100, nullable = false)
+    private  String content;
+
+    @Column(nullable = false)
+    private String category;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+    
 }
