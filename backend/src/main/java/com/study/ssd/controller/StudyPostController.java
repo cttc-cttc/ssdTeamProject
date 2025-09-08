c0678231c766c7c005d41e6f45af039ca645af20
dhrxh99 <dhrxhan@gmail.com>
2025-09-08 15:31:52 +0900

    studyController

---
+++ backend/src/main/java/com/study/ssd/controller/StudyPostController.java
@@ -0,0 +1,27 @@
+package com.study.ssd.controller;
+
+import com.study.ssd.dto.StudyPostRequest;
+import com.study.ssd.dto.StudyPostResponse;
+import com.study.ssd.repository.StudyPostRepository;
+import com.study.ssd.service.StudyPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posting")
+@RequiredArgsConstructor
+public class StudyPostController {
+
+    private  final StudyPostService  studyPostService;
+
+    /*@PostMapping
+    public ResponseEntity<StudyPostResponse> createPost (@RequestBody StudyPostRequest studyPostRequest) {
+
+        StudyPostResponse studyPostResponse = studyPostService.crea(studyPostRequest);
+        return ResponseEntity.ok(studyPostResponse);
+    }*/
+}
---
+++ frontend/src/pages/studyPost/postingForm.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function PostingForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [mainCategory, setMainCategory] = useState("");
+  const [maxCount, setMaxCount] = useState("");
+}
