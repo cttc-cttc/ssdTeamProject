1f2a5bd1b3c2c8bff8c0be8b2971de9308fb87ee
kwy <kwy1762@naver.com>
2025-09-11 21:57:05 +0900

    주석 삭제

--- frontend/src/pages/home/components/list-thumbnail-grid.tsx
+++ frontend/src/pages/home/components/list-thumbnail-grid.tsx
@@ -10,34 +10,6 @@ export default function ListThumbnailGrid({ study }: { study: studyProps }) {
   const firstImage = match ? match[1] : null; // 이미지 url
 
   return (
-    // <div className="shadow-xl border-1 border-accent max-h-72">
-    //   {firstImage ? (
-    //     <img src={firstImage} alt="thumbnail" className="w-full h-96 object-cover" />
-    //   ) : (
-    //     <ImageFrame />
-    //   )}
-    //   <div className="flex flex-col gap-1 p-3 bg-white dark:bg-muted/50">
-    //     <div className="flex justify-between">
-    //       <div className="text-[#2c3e50] dark:text-accent-foreground font-bold text-lg">
-    //         {study.title}
-    //       </div>
-    //       <div>
-    //         참가 인원 {study.currentCont}/{study.maxCount}
-    //       </div>
-    //     </div>
-    //     <div className="flex justify-between">
-    //       <div className="dark:text-muted-foreground">
-    //         카테고리 - {categoryNameMap[study.category]}
-    //       </div>
-    //       <div className="dark:text-muted-foreground">찜하기 {study.wishCount}</div>
-    //     </div>
-    //     <div className="dark:text-muted-foreground">
-    //       스터디 기간 {dayjs(study.created).format("YYYY-MM-DD")} ~{" "}
-    //       {dayjs(study.deadline).format("YYYY-MM-DD")}
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="shadow-xl border border-accent max-h-80 rounded overflow-hidden flex flex-col">
       {/* 이미지 영역 */}
       <div className="w-full h-60 overflow-hidden">
