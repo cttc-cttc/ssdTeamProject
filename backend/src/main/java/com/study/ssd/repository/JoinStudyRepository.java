7c62a6a976c384e8789923014409c29951af5404
dhrxh99 <dhrxhan@gmail.com>
2025-09-18 11:59:55 +0900

    개설 스터디 정렬

--- backend/src/main/java/com/study/ssd/repository/JoinStudyRepository.java
+++ backend/src/main/java/com/study/ssd/repository/JoinStudyRepository.java
@@ -14,7 +14,7 @@ public interface JoinStudyRepository extends JpaRepository<JoinStudy, Long> {
     Optional<JoinStudy> findByUserIdAndPostId(Long userId, Long postId);
     
     // 특정 사용자가 참여한 모든 스터디 조회
-    @Query("SELECT js FROM JoinStudy js JOIN FETCH js.post WHERE js.user.id = :userId")
+    @Query("SELECT js FROM JoinStudy js JOIN FETCH js.post WHERE js.user.id = :userId order by js.id DESC ")
     List<JoinStudy> findByUserIdWithPost(@Param("userId") Long userId);
     
     // 특정 스터디에 참여한 사용자 수 조회
