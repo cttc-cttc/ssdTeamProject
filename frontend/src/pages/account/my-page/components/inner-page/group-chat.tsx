67fe578d1f8a98e42df1380cf434fe659429650f
kwy <kwy1762@naver.com>
2025-09-20 04:35:02 +0900

    참여 스터디에서 자신이 다른 사람이 개설한 스터디의 채팅방을 만들 수 없도록 수정

--- frontend/src/pages/account/my-page/components/inner-page/group-chat.tsx
+++ frontend/src/pages/account/my-page/components/inner-page/group-chat.tsx
@@ -16,6 +16,7 @@ interface RoomData {
 export default function GroupChat() {
   const { id } = useParams();
   const { userNickname, userPkID } = useInfoStore();
+  const [studyOpener, setStudyOpener] = useState(null);
   const [message, setMessage] = useState("");
   const [isExistRoom, setIsExistRoom] = useState(false);
   const [currentRoomId, setCurrentRoomId] = useState<string | null>(null);
@@ -43,6 +44,8 @@ export default function GroupChat() {
     const checkStudyStatus = async () => {
       setLoading(true);
       const response = await axios.get(`/api/create-post/${id}`);
+      setStudyOpener(response.data.userNickname);
+
       if (response.data.ended) {
         setMessage("종료된 스터디는 채팅방에 입장할 수 없습니다.");
         setLoading(false);
@@ -57,6 +60,18 @@ export default function GroupChat() {
   // CreateGroupChat에서 생성된 방을 받아 상태 변경
   const onSelectRoom = (roomId: string) => {
     setCurrentRoomId(roomId);
+  };
+
+  const studyOpenerRender = () => {
+    if (studyOpener === userNickname) {
+      return <CreateGroupChat onSelectRoom={onSelectRoom} postId={id} />;
+    } else {
+      return (
+        <p className="text-center text-xl text-[#FE9CA2] font-semibold">
+          아직 채팅방이 생성되지 않았습니다.
+        </p>
+      );
+    }
   };
 
   // 뒤로가기
@@ -80,8 +95,9 @@ export default function GroupChat() {
             {message !== "" ? (
               <p className="text-center text-xl text-destructive font-semibold">{message}</p>
             ) : !isExistRoom ? (
-              // 방이 없으면 새로 생성
-              <CreateGroupChat onSelectRoom={onSelectRoom} postId={id} />
+              // 방이 없으면 스터디 개설자인 경우만 새로 생성
+              // 스터디 개설자가 아니면 "아직 채팅방이 생성되지 않았습니다." 안내문 출력
+              studyOpenerRender()
             ) : (
               // 방 정보와 로그인 유저 정보가 있으면 바로 채팅방 로드
               currentRoomId &&
